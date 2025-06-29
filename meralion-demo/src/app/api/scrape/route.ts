3a2416bcb61c47701e605f21c963cc0c5e9ed08e
Soh Hong Yu <sohhongyu@gmail.com>
2025-06-30 07:32:51 +0800

    Testing Vercel Timeout

--- meralion-demo/src/app/api/scrape/route.ts
+++ meralion-demo/src/app/api/scrape/route.ts
@@ -262,6 +262,9 @@ export async function POST(req: Request) {
         }
       );
     }
+
+    await new Promise((resolve) => setTimeout(resolve, 10000));
+
     const lastMessageData = await page.evaluate((el: HTMLElement) => {
       const audioElement = el.querySelector("audio") as HTMLAudioElement | null;
       return {
