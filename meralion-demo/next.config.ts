73eb6f5bf7ae01fb0acfb66d988477b766301def
Soh Hong Yu <sohhongyu@gmail.com>
2025-06-29 23:25:42 +0800

    Update next.config.ts

--- meralion-demo/next.config.ts
+++ meralion-demo/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingIncludes: {
+    "/api/scrape": ["node_modules/@sparticuz/chromium/**"],
+  },
 };
 
 export default nextConfig;
